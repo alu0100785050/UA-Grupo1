5eea88d01ccb8a195aa387fddd66c5079136ecb8
diego-algomas <alu0101029993@ull.edu.es>
2019-03-19 11:21:29 +0000

    imprime por pantalla

--- Intro-Javascript/cambiomonedas.js
+++ Intro-Javascript/cambiomonedas.js
@@ -1,7 +1,12 @@
  function cambio(){
     var cambio={apeso:27.68,aeuro:0.88,alibra:0.75};
     var dolar=3;
-    var peso=dolar*cambio[apeso];
-    var euro=dolar*cambio[aeuro];
-    var libra=dolar*cambio[alibra];
+    var peso=dolar*cambio['apeso'];
+    var euro=dolar*cambio['aeuro'];
+    var libra=dolar*cambio['alibra'];
+    console.log("Cantidad de dolares a cambiar: " +dolar);
+    console.log("Cantidad en pesos: "+ peso);
+    console.log("Cantidad en euros: "+euro);
+    console.log("Cantidad en libras: "+ libra);
 }
+cambio();
