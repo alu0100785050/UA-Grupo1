f0d473ba533af0d8f492faba76c6a5cbe67f0d68
diego-algomas <38524810+diego-algomas@users.noreply.github.com>
2019-03-19 11:12:00 +0000

    done

---
+++ Intro-Javascript/cambiomonedas.js
@@ -0,0 +1,7 @@
+ function cambio(){
+    var cambio={apeso:27.68,aeuro:0.88,alibra:0.75};
+    var dolar=3;
+    var peso=dolar*cambio[apeso];
+    var euro=dolar*cambio[aeuro];
+    var libra=dolar*cambio[alibra];
+}
