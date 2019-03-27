476ce6e4e727ef8dbaf5a2e212db96789ee3c75d
Óscar González García <oscar.glez.97@hotmail.com>
2019-03-27 21:27:15 +0000

    mejora del igic

--- Intro-Javascript/igic.js
+++ Intro-Javascript/igic.js
@@ -1,7 +1,16 @@
-function igicm(ValNum){
 
+var precios={1:0,2:0,3:0,4:0};
+
+function igicm(ValNum,i){
+
+	precios[i]=ValNum;
     let valigic=0.065;
-	let total=valigic*ValNum+parseInt(ValNum,10);
+	var total=0;
+	
+	for(var item in precios){
+		total+=(precios[item]*valigic)+ parseInt(precios[item],10);
+		console.log(precios[item]);
+	}
 	document.getElementById("igic").innerHTML=total;
 	
 }
