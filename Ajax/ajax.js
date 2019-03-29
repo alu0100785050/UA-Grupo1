937fe5fea383c799ec5efdcc6efbd5654dc7d9c1
diego-algomas <alu0101029993@ull.edu.es>
2019-03-29 18:40:32 +0000

    ajax

--- Ajax/ajax.js
+++ Ajax/ajax.js
@@ -2,18 +2,20 @@
 		$.ajax({
 			url:'https://jsonplaceholder.typicode.com/comments?postId=1',
 			type: "get",
-			dataType: "jsonp",
+			dataType: "json",
 			headers: {'Access-Control-Allow-Origin': '*'},
 			async: true,
 			data: 'parametro1=valor1&parametro2=valor2',
 			success: function(result){
 				alert("ok");
-				$('#exito').append(`<p>${result}</p>`);
-				},
-			error: function(result){
+				
+				for(let i=0;i<result.length;i++){
+					let st= JSON.stringify(result[i])
+					$('#exito').append(`${st}<br>`);
+				}
+			},
+			error: function(...result){
 				alert('bad')	;
-				$("#fracaso").load();
-
 				}	
 		})
 	});	
