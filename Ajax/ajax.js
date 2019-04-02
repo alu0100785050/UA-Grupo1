636824c4a15cebb0ed45abbaab16c39c9aca043b
diego-algomas <alu0101029993@ull.edu.es>
2019-04-02 11:01:19 +0100

    modifiacion

--- Ajax/ajax.js
+++ Ajax/ajax.js
@@ -1,17 +1,16 @@
 	$("#boton").click(function(e){
 		$.ajax({
-			url:'https://jsonplaceholder.typicode.com/comments?postId=1',
+			url:`https://jsonplaceholder.typicode.com/comments?postId=${$("#valor").val()}`,
 			type: "get",
 			dataType: "json",
 			headers: {'Access-Control-Allow-Origin': '*'},
 			async: true,
-			data: 'parametro1=valor1&parametro2=valor2',
+			
 			success: function(result){
 				alert("ok");
 				
 				for(let i=0;i<result.length;i++){
-					let st= JSON.stringify(result[i])
-					$('#exito').append(`${st}<br>`);
+					$('#exito').append(` name : ${result[i].name} email: ${result[i].email}<br><br>`);
 				}
 			},
 			error: function(...result){
