721fa09d591bfbbda389d7ad7310c0817d386478
diego-algomas <alu0101029993@ull.edu.es>
2019-04-23 16:20:18 +0100

    modifica pero no añade

--- web/public/js/firebase.js
+++ web/public/js/firebase.js
@@ -7,3 +7,26 @@ var config = {
   messagingSenderId: "175720945121"
 };
 firebase.initializeApp(config);
+
+
+var book = firebase.database().ref("BOOKS");
+var nbook = function(){
+  
+  var title = $("#title").val();
+  var category = $("#category").val();
+  var autor= $("#author").val();
+  var isbn = $("#isbn").val(); 
+  book.set({
+    title: title,
+    category: category,
+    author: autor,
+    isbn: isbn
+  })
+
+}
+
+
+$(window).load(function () {
+  $("#formulario").submit(nbook);
+
+});
