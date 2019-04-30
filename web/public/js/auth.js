4d86e3bb5f703afcea50e00c45d5209e019792e7
diego-algomas <alu0101029993@ull.edu.es>
2019-04-30 11:29:37 +0100

    nuevo

--- web/public/js/auth.js
+++ web/public/js/auth.js
@@ -10,13 +10,27 @@ firebase.initializeApp(config);
 
 document.getElementById("register").addEventListener("click",function(){
     var user = firebase.database().ref("USERS");
-    alert($("#scorreo").val())
     firebase.auth().createUserWithEmailAndPassword($("#scorreo").val(),$("#spassword").val()).catch(function(error){
         alert(error);
     });
+    firebase.auth().singInWithEmailAndPassword($("#scorreo").val(),$("#spassword").val());
+    var current = firebase.auth().currentUser;
+
+    var username = $("#susername").val();
+    current.updateProfile({
+        displayName: username, 
+      }).then(function() {
+        alert("correct")
+      }).catch(function(error) {
+        alert("bad luck")
+      });
+      
+    // user.child(firebase.auth().uid().set({
+    //     username: username
+    // }));
 });
 document.getElementById("login").addEventListener("click",function(){
-    firebase.auth().singInWithEmailAndPassword($("#correo").val(),$("#password").val()).catch(function(error){
+    firebase.auth().singInWithEmailAndPassword($("#lcorreo").val(),$("#lpassword").val()).catch(function(error){
         alert(error);
     });
 });
